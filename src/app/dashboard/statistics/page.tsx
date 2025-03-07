4e27bb842f49be4712608e72fa887f976398e44c
DSantiagG <davidgiron@unicauca.edu.co>
2025-03-07 12:44:28 -0500

    Añadir componente Chart

--- src/app/dashboard/statistics/page.tsx
+++ src/app/dashboard/statistics/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Title from "@/components/ui/title";
-import BarChartMobilityByFaculty from "@/components/statistics/barChart/BarChartMobilityByFaculty";
-import BarChartMobilityByCountry from "@/components/statistics/barChart/BarChartMobilityByCountry";
-//import BarChartMobilitiesPerYear from "@/components/statistics/barChart/BarChartMobilityPerYear";
-import BarChartMobilityByEvent from "@/components/statistics/barChart/BarChartMobilityByEvent";
+import Chart from "@/components/statistics/chart";
+import BarChartMobilityByFaculty from "@/components/statistics/barChart/barChartMobilityByFaculty";
+import BarChartMobilityByCountry from "@/components/statistics/barChart/barChartMobilityByCountry";
+//import BarChartMobilitiesPerYear from "@/components/statistics/barChart/barChartMobilityPerYear";
+import BarChartMobilityByEvent from "@/components/statistics/barChart/barChartMobilityByEvent";
 import LineChartMobilityTrend from "@/components/statistics/lineChart/LineChartMobilityTrend";
 import PieChartMobilityByEvent from "@/components/statistics/pieChart/PieChartMobilityByEvent";
-
 
 export default function StatisticsPage() {
 
@@ -16,54 +16,24 @@ export default function StatisticsPage() {
             <Title title="Estadísticas" />
             
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mx-10 my-10">
-                <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-xl">
-                    <div className="flex justify-center mt-5">
-                        <h2>Movilidad por facultad</h2>
-                    </div>
-                    <div className="mt-5 h-full"> 
-                        <BarChartMobilityByFaculty />
-                    </div>
-                </div>
-                <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-xl">
-                    <div className="flex justify-center mt-5">
-                        <h2>Movilidad por paises</h2>
-                    </div>
-                    <div className="mt-5 h-full">
-                        <BarChartMobilityByCountry />
-                    </div>
-                </div>
-                {/*<div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-xl">
-                    <div className="flex justify-center mt-5">
-                        <h2>Movilidades por año</h2>
-                    </div>
-                    <div className="mt-5 h-full">
-                        <BarChartMobilitiesPerYear />
-                    </div>
-                </div>*/}
-                <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-xl">
-                    <div className="flex justify-center mt-5">
-                        <h2>Movilidades por Evento</h2>
-                    </div>
-                    <div className="mt-5 h-full">
-                        <BarChartMobilityByEvent />
-                    </div>
-                </div>
-                <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-xl">
-                    <div className="flex justify-center mt-5">
-                        <h2>Tendencia de movilidad anual</h2>
-                    </div>
-                    <div className="mt-5 h-full">
-                        <LineChartMobilityTrend />
-                    </div>
-                </div>
-                <div className="border border-gray-100 bg-white rounded-2xl h-96 shadow hover:shadow-xl">
-                    <div className="flex justify-center mt-5">
-                        <h2>Distribución por tipos de evento</h2>
-                    </div>
-                    <div className="mt-5 h-full">
-                        <PieChartMobilityByEvent />
-                    </div>
-                </div>
+                <Chart title="Movilidad por facultad">
+                    <BarChartMobilityByFaculty />
+                </Chart>
+                <Chart title="Movilidad por paises">
+                    <BarChartMobilityByCountry />
+                </Chart>
+                {/*<Chart title="Movilidades por año">
+                    <BarChartMobilitiesPerYear />
+                </Chart>*/}
+                <Chart title="Movilidades por Evento">
+                    <BarChartMobilityByEvent />
+                </Chart>
+                <Chart title="Tendencia de movilidad anual">
+                    <LineChartMobilityTrend />
+                </Chart>
+                <Chart title="Distribución por tipos de evento">
+                    <PieChartMobilityByEvent />
+                </Chart>
             </div> 
         </>
     );
